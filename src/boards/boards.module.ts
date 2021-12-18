86c1f079b7d8a1c129b99459d295d3f716edad61
khaki4 <4dakto@gmail.com>
2021-12-18 17:22:37 +0900

    add getAllBoard handler

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { BoardsService } from './boards.service';
 
 @Module({
-  providers: [BoardsService]
+  providers: [],
 })
 export class BoardsModule {}

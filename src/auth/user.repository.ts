96bc8e9491d02d7b2877a978e4c7924b539f0226
khaki4 <4dakto@gmail.com>
2021-12-19 11:32:21 +0900

    회원가입 기능 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<User> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+
+    return user;
+  }
+}
